471670b7c49574d6e941f028bed750b64c3b0ec2
Fabian Beuke <mail@beuke.org>
2017-05-23 12:30:54 +0200

    add zsh vi mode indicator

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -56,6 +56,21 @@ bindkey . rationalise-dot
 # zle -N ale-line-finish
 # zle -N zle-keymap-select
 
+
+# if mode indicator wasn't setup by theme, define default
+if [[ "$MODE_INDICATOR" == "" ]]; then
+  MODE_INDICATOR="%{$fg_bold[red]%}<%{$fg[red]%}<<%{$reset_color%}"
+fi
+
+function vi_mode_prompt_info() {
+  echo "${${KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/}"
+}
+
+# define right prompt, if it wasn't defined by a theme
+if [[ "$RPS1" == "" && "$RPROMPT" == "" ]]; then
+  RPS1='$(vi_mode_prompt_info)'
+fi
+
 # right prompt settings
 local timing='$(printf "%%{$fg[cyan]%%}%.2f%%f" "$ZSH_COMMAND_TIME")'
 error="%{$fg[red]%}%(?..%? )"

82a97a421c58111692c739c42c2fe92f911d2faa
Fabian Beuke <mail@beuke.org>
2017-07-21 14:03:11 +0200

    update conky startup script

--- scripts/start-conky.hs
+++ scripts/start-conky.hs
@@ -6,31 +6,29 @@ import Turtle
 import Network.Wreq
 import qualified Data.Text as T
 import Control.Lens
-
-getStatus :: Response body -> Int
-getStatus = (^. (responseStatus . statusCode))
-
-hasInternet :: Int -> Bool
-hasInternet = (==) 200
+import Data.Text.ICU.Replace
 
 processIsRunning :: String -> IO ExitCode
-processIsRunning = flip shell empty . T.pack . ("ps x | /usr/bin/grep " ++)
+processIsRunning = flip shell empty . T.pack . ("ps x | /usr/bin/grep " ++) . grepFormat
+            where grepFormat = T.unpack . replaceAll "^." "[$0]" . T.pack
 
 startConky ::  String -> IO ExitCode
 startConky = flip shell empty . T.pack . ("conky -c ~/.config/conky/" ++)
 
+conky :: String -> IO ()
+conky s = do
+     exit <- processIsRunning s
+     case exit of
+         ExitSuccess   -> print ("conky " ++ s ++ " already running")
+         ExitFailure n -> print =<< startConky (s ++  " &")
+
+hasInternet :: IO Bool
+hasInternet = do
+    r <- get "https://google.com"
+    return $ (==) 200 (r ^. responseStatus . statusCode)
+
 main :: IO ()
 main = do
-    r <- get "https://google.com"
-    let online = hasInternet $ getStatus r
-    if online then do
-        exit <- processIsRunning "[w]eather"
-        case exit of
-            ExitSuccess   -> print "conky weather already running"
-            ExitFailure n -> print =<< startConky "weather &"
-        exit <- processIsRunning "[e]rrors"
-        case exit of
-            ExitSuccess   -> print "conky error already running"
-            ExitFailure n -> print =<< startConky "errors &"
-     else
-        main
+    online <- hasInternet
+    if online then mapM_ conky ["weather", "errors"] else main
+

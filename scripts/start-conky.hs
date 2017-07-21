f21a1a0ad9b5e1537a390f45f12912a2a8570a74
Fabian Beuke <mail@beuke.org>
2017-07-22 00:40:47 +0200

    update conky startup script

--- scripts/start-conky.hs
+++ scripts/start-conky.hs
@@ -1,20 +1,23 @@
 #!/usr/bin/env stack
--- stack --install-ghc runghc --package turtle --package http-conduit
+-- stack --install-ghc runghc turtle wreq http-conduit string-conversions
 
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
 
 import Turtle
 import Network.Wreq
 import Control.Lens
 import Data.Text.ICU.Replace
-import qualified Data.Text as T
+import System.Process (readProcess)
+import Data.String.Conversions (cs)
 
 processIsRunning :: String -> IO ExitCode
-processIsRunning = flip shell empty . T.pack . ("ps x | /usr/bin/grep " ++) . grepFormat
-            where grepFormat = T.unpack . replaceAll "^." "[$0]" . T.pack
+processIsRunning = flip shell empty . cs . ("ps x | /usr/bin/grep " ++) . grepFormat
+    where grepFormat = cs . replaceAll "^." "[$0]" . cs
 
 startConky ::  String -> IO ExitCode
-startConky = flip shell empty . T.pack . ("conky -c ~/.config/conky/" ++)
+startConky = flip shell empty . cs . ("conky -c ~/.config/conky/" ++)
 
 conky :: String -> IO ()
 conky name = do
@@ -28,8 +31,16 @@ hasInternet = do
     response <- get "https://google.com"
     return $ (==) 200 (response ^. responseStatus . statusCode)
 
+sidebar :: IO ()
+sidebar = do
+    hostname <- readProcess "hostname" [] ""
+    case hostname of
+      "arch\n" -> conky "conkyrc"
+      "skylake\n" -> conky "conkyrc-work"
+      _ -> echo "unkown host"
+
 main :: IO ()
 main = do
+    sidebar
     online <- hasInternet
     if online then mapM_ conky ["weather", "errors"] else main
-

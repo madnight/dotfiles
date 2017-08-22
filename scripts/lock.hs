07bf73da02d794fe55943074892686401ede981a
Fabian Beuke <mail@beuke.org>
2017-08-22 21:01:03 +0200

    add comments to lock.hs

--- scripts/lock.hs
+++ scripts/lock.hs
@@ -15,9 +15,13 @@ main = do
     args <- getArgs
     let icon = parseArgs $ listToMaybe args
     let tmp = "/tmp/screen.png"
+    -- take screenshot
     cmd "scrot" [tmp]
+    -- blur the screenshot
     cmd "convert" [tmp, "-scale", "10%", "-scale", "1000%", tmp]
+    -- add lock image to the center of the blured screenshot
     cmd "convert" [tmp, icon, "-gravity", "center", "-composite", "-matte", tmp]
+    -- call i3 with lock screen image
     cmd "i3lock" ["-i",  tmp]
     where
         cmd = (void .) . rawSystem

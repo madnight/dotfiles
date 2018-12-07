b6492ac00c589e84cb11af51dea34bbb080a1a60
Fabian Beuke <mail@beuke.org>
2018-12-07 20:47:35 +0100

    add weather.py

---
+++ scripts/weather.py
@@ -0,0 +1,23 @@
+#!/bin/python
+
+import os
+import subprocess
+import urllib.request, json
+
+city = "Bremen"
+# this api key is intentionally exposed to public on github since
+# openwathermap api keys can be generated freely for everyone
+pub_api_key = "27dea0433cf24e341ea548fdd013403f"
+units = "Metric"
+unit_key = "C"
+
+try:
+    weather = eval(str(urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units={}".format(city, pub_api_key, units)).read())[2:-1])
+    info = weather["weather"][0]["description"].capitalize()
+    temp = int(float(weather["main"]["temp"]))
+    print("%s, %i °%s" % (info, temp, unit_key))
+except:
+    bashCommand = "bash " + os.environ['HOME'] + "/scripts/weather.bash"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(output)

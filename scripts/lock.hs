7a358873f5b94b52636d43c2521d765ba2cfa977
Fabian Beuke <mail@beuke.org>
2019-02-01 13:27:59 +0100

    update lock.hs

--- scripts/lock.hs
+++ scripts/lock.hs
@@ -1,6 +1,3 @@
-#!/usr/bin/env stack
--- stack --install-ghc runghc
-
 import Control.Monad
 import Data.Maybe
 import System.Environment
@@ -8,18 +5,43 @@ import System.Process
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let icon = parseArgs $ listToMaybe args
-    let tmp = "/tmp/screen.png"
+  home <- getEnv "HOME"
+  let icon = home ++ "/scripts/lock.png"
+  let tmp = "/tmp/screen.png"
     -- | take screenshot
-    cmd "scrot" [tmp]
+  cmd "scrot" [tmp]
     -- | blur the screenshot
-    cmd "convert" [tmp, "-scale", "10%", "-scale", "1000%", tmp]
+  cmd "convert" [tmp, "-scale", "10%", "-scale", "1000%", tmp]
     -- | add lock image to the center of the blured screenshot
-    cmd "convert" [tmp, icon, "-gravity", "center", "-composite", "-matte", tmp]
+  cmd "convert" [tmp, icon, "-gravity", "center", "-composite", "-matte", tmp]
     -- | call i3 with lock screen image
-    cmd "i3lock" ["-i",  tmp]
-    where
-        cmd = (void .) . rawSystem
-        parseArgs Nothing  = errorWithoutStackTrace "missing arg: lock picture"
-        parseArgs (Just x) = x
+  cmd
+    "i3lock"
+    [ "-i"
+    , tmp
+    , "--insidevercolor=#ffffff00"
+    , "--ringvercolor=#0000bb00"
+    , "--insidewrongcolor=#CF42C100"
+    , "--ringwrongcolor=#CF42C1ff"
+    , "--insidecolor=#eeeeee00"
+    , "--ringcolor=#00000000"
+    , "--linecolor=#D648C3aa"
+    , "--separatorcolor=#ffffffff"
+    , "--verifcolor=#ffffffaa"
+    , "--wrongcolor=#CF42C1ff"
+    , "--timecolor=#ee00e0ee"
+    , "--datecolor=#ee00e0ee"
+    , "--layoutcolor=#000000ff"
+    , "--keyhlcolor=#00DBF1ff"
+    , "--bshlcolor=#00DBF1ff"
+    , "--ring-width=10"
+    , "--screen=1"
+    , "--radius=180"
+    , "--wrongtext=''"
+    , "--veriftext=''"
+    , "--noinputtext=''"
+    , "--locktext=''"
+    , "--lockfailedtext=''"
+    ]
+  where
+    cmd = (void .) . rawSystem

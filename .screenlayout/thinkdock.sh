f06e9798a89939e72ea0e4abb059c17b73f19265
Fabian Beuke <mail@beuke.org>
2019-06-06 13:07:14 +0200

    update dots

--- .screenlayout/thinkdock.sh
+++ .screenlayout/thinkdock.sh
@@ -6,8 +6,8 @@ xrandr               \
 --output HDMI1 --off \
 --output VGA1  --off
 
-xrandr --output HDMI3 --mode 1680x1050 --pos 0x30 --rotate normal
-xrandr --output VGA1 --mode 1680x1050 --pos 3600x0 --rotate normal
+xrandr --output VGA1 --mode 1680x1050 --pos 0x0 --rotate normal
+xrandr --output HDMI3 --mode 1680x1050 --pos 3600x30 --rotate normal
 sleep 2
 xrandr --output HDMI2 --primary --mode 1920x1080 --pos 1680x0 --rotate normal
 

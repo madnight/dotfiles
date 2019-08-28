25f550b0eaca2b4831926b324b1c569707d81e08
Fabian Beuke <mail@beuke.org>
2019-08-28 13:47:55 +0200

    tweak lock.go

--- scripts/lock.go
+++ scripts/lock.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-    "github.com/nfnt/resize"
     "github.com/vova616/screenshot"
+    "github.com/disintegration/gift"
     "image/png"
-    "image/draw"
     "image"
     "os"
     "bytes"
@@ -12,31 +11,28 @@ import (
 )
 
 func main() {
+
+    lockChan := make(chan image.Image)
+
     // read lock icon file
-    fdLock, _ := os.Open("/home/x/scripts/lock.png")
-    pngLock, _ := png.Decode(fdLock)
-    lock := image.Image(pngLock)
+    go func() {
+        fdLock, _ := os.Open("/home/x/scripts/lock.png")
+        pngLock, _ := png.Decode(fdLock)
+        lockChan <- image.Image(pngLock)
+    }()
 
     // make screenshot
     raw_root, _ := screenshot.CaptureScreen()
-    screenshot := image.Image(raw_root)
 
     // pixelate
-    screenshot = resize.Resize(1920/10, 0, screenshot, resize.NearestNeighbor)
-    screenshot = resize.Resize(1920, 0, screenshot, resize.NearestNeighbor)
+    gift.New(gift.Pixelate(10)).Draw(raw_root, raw_root)
 
     // paste lock onto screenshot
-    bg := image.NewRGBA(screenshot.Bounds())
-    draw.Draw(bg, screenshot.Bounds(), screenshot, image.ZP, draw.Src)
-    draw.Draw(bg, lock.Bounds().Add(image.Pt(600, 400)), lock, image.ZP, draw.Over)
+    gift.New().DrawAt(raw_root, <-lockChan, image.Pt(600, 400), gift.OverOperator)
 
-    // convert image to byte buffer
-    var b bytes.Buffer
-    b.Write([]byte(bg.Pix))
-
-    // os.Exit(0)
+    os.Exit(0)
     // run i3lock with raw byte buffer as stdin
     i3 := exec.Command("i3lock", "--raw", "1920x1080:rgbx", "-i", "/dev/stdin")
-    i3.Stdin = &b
+    i3.Stdin = bytes.NewBuffer([]byte(raw_root.Pix))
     i3.Run()
 }

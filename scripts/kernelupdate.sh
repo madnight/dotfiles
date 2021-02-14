aaf4b02f436d9a452b56e4244cd0f5577fa76b48
Fabian Beuke <mail@beuke.org>
2021-02-14 20:43:11 +0100

    update kernelupdate script

--- scripts/kernelupdate.sh
+++ scripts/kernelupdate.sh
@@ -2,7 +2,7 @@
 
 while sleep 10m
 do
-    if [[ $(pacman -Q linux-lts | cut -d " " -f 2) > $(uname -r) ]]; then
+    if [[ $(pacman -Q linux | cut -d " " -f 2) > $(uname -r) ]]; then
         notify-send "Kernel Update!" "Reboot required!"
     fi
 done

d7ad506b386e5f21a1318195db75a9734b51c11d
Fabian Beuke <mail@beuke.org>
2021-03-27 14:27:50 +0100

    update dots

--- scripts/kernelupdate.sh
+++ scripts/kernelupdate.sh
@@ -2,7 +2,7 @@
 
 while sleep 10m
 do
-    if [[ $(pacman -Q linux | cut -d " " -f 2) > $(uname -r) ]]; then
+    if [[ "$(pacman -Q linux | cut -d " " -f 2)" == *"$(uname -r | cut -d- -f2)"* ]]; then
         notify-send "Kernel Update!" "Reboot required!"
     fi
 done

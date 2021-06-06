65eda5d8cfc0ad9d4a883c34d64f677fb9f073a2
Fabian Beuke <mail@beuke.org>
2021-06-06 20:51:59 +0200

    remove travis

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-(
-# find all shell scripts and run `shellcheck`
-find . -regex ".*\.\(zsh\|sh\|bash\|zshrc\)" -print0 \
-| while IFS= read -r -d $'\0' script; do
-	shellcheck "$script"
-done
-) || true

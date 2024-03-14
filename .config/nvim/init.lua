6c4d4d2fb59d3c1dc5d78f6f48421417ec4d9564
Fabian Beuke <mail@beuke.org>
2024-03-15 00:04:14 +0100

    use more lua like syntax for nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
-vim.cmd([[
+local Plug = vim.fn['plug#']
 
-call plug#begin()
+vim.call('plug#begin')
+
 Plug 'w0ng/vim-hybrid'
 Plug 'nvim-lua/popup.nvim'
 Plug 'nvim-telescope/telescope.nvim'
@@ -8,134 +9,103 @@ Plug 'nvim-lua/plenary.nvim'
 Plug 'MunifTanjim/nui.nvim'
 Plug 'alexghergh/nvim-tmux-navigation'
 
-
-" #####################
-" always active plugins
-" #####################
-
-" an ide like file explorer
+-- an ide like file explorer
 Plug 'nvim-tree/nvim-tree.lua'
 
-" editorconfig define consistent coding styles in different editors
+-- editorconfig define consistent coding styles in different editors
 Plug 'editorconfig/editorconfig-vim'
 
-" incsearch.vim incrementally highlights ALL pattern matches
+-- incsearch.vim incrementally highlights ALL pattern matches
 Plug 'haya14busa/incsearch.vim'
 
-" camel case jumps with w
+-- " camel case jumps with w
 Plug 'bkad/CamelCaseMotion'
 
-" show trailing whitespaces
+-- " show trailing whitespaces
 Plug 'bronson/vim-trailing-whitespace'
 
-" provide easy code formatting in Vim by integrating existing code formatters
+-- " provide easy code formatting in Vim by integrating existing code formatters
 Plug 'Chiel92/vim-autoformat'
 
-" enhanced vim diff
+-- " enhanced vim diff
 Plug 'chrisbra/vim-diff-enhanced'
 
-" vim script for text filtering and alignment
+-- " vim script for text filtering and alignment
 Plug 'godlygeek/tabular'
 
-" allow atom like line swapping with arrow keys
+-- " allow atom like line swapping with arrow keys
 Plug 'madnight/vim-swap-lines'
 
-" vim sugar for the UNIX shell commands that need it the most
+-- " vim sugar for the UNIX shell commands that need it the most
 Plug 'tpope/vim-eunuch'
 
-" git wrapper that should be illegal
+-- " git wrapper that should be illegal
 Plug 'tpope/vim-fugitive'
 
-" minimal common sense vim tweaks
+-- " minimal common sense vim tweaks
 Plug 'tpope/vim-sensible'
 
-" automatically adjusts 'shiftwidth' and 'expandtab' heuristically based
+-- " automatically adjusts 'shiftwidth' and 'expandtab' heuristically based
 Plug 'tpope/vim-sleuth'
 
-" add parentheses arround current word or sentence
+-- " add parentheses arround current word or sentence
 Plug 'tpope/vim-surround'
 
-" add useful extra commands
+-- " add useful extra commands
 Plug 'tpope/vim-unimpaired'
 
-" vim status line
+-- " vim status line
 Plug 'vim-airline/vim-airline'
 
-" vim tmux resize integration
-" Plug 'madnight/vim-tmux-resizer'
-
-" vim status line themes
+-- " vim status line themes
 Plug 'vim-airline/vim-airline-themes'
 
-" hybrid theme https://i.imgur.com/M3Qthm4.png
+-- " hybrid theme https://i.imgur.com/M3Qthm4.png
 Plug 'w0ng/vim-hybrid'
 
-" syntastic replacement that uses background threads for syntax check
-" Plug 'w0rp/ale'
-
-" comment out stuff via shortcut
+-- " comment out stuff via shortcut
 Plug 'scrooloose/nerdcommenter'
 
-" wrapper for fuzzy findec
+-- " wrapper for fuzzy findec
 Plug 'junegunn/fzf.vim'
 
-" fuzzy file, buffer, mru, tag, etc finder with rg backend
+-- " fuzzy file, buffer, mru, tag, etc finder with rg backend
 Plug 'junegunn/fzf'
 
-" shows a git diff in the 'gutter' (sign column)
+-- " shows a git diff in the 'gutter' (sign column)
 Plug 'airblade/vim-gitgutter'
 
-" fix gui only colorschemes to work in terminal
+-- " fix gui only colorschemes to work in terminal
 Plug 'godlygeek/csapprox'
 
-" FocusGained and FocusLost autocommand events for tmxux
+-- " FocusGained and FocusLost autocommand events for tmxux
 Plug 'tmux-plugins/vim-tmux-focus-events'
 
-" A Vim plugin for more pleasant editing on commit messages
+-- " A Vim plugin for more pleasant editing on commit messages
 Plug 'rhysd/committia.vim'
 
-" Interactive command execution in Vim.
-Plug 'shougo/vimproc.vim', {'do' : 'make'}
+-- " Interactive command execution in Vim.
+Plug 'shougo/vimproc.vim'
 
-" Changes Vim working directory to project root
+-- " Changes Vim working directory to project root
 Plug 'airblade/vim-rooter'
 
-" Autoformat Haskell
-Plug 'alx741/vim-hindent'
+-- " Autoformat JS
+Plug 'prettier/vim-prettier'
 
-" Autoformat JS
-Plug 'prettier/vim-prettier', { 'do': 'yarn install' }
-
-" Highlight the exact differences, based on characters and words
+-- " Highlight the exact differences, based on characters and words
 Plug 'rickhowe/diffchar.vim'
 
-" a collection of language packs for Vim.
+-- " a collection of language packs for Vim.
 Plug 'sheerun/vim-polyglot'
 
-" Intellisense engine for vim8 & neovim, full language server protocol support
-"Plug 'neoclide/coc.nvim', {'do': { -> coc#util#install()}}
-" Plug 'neoclide/coc.nvim', {'branch': 'release'}
-
-" ################################
-" # on command lazy loaded plugins
-" ################################
-
-" colorized filenames in nerdtree
-Plug 'tiagofumo/vim-nerdtree-syntax-highlight', { 'on': 'NERDTreeToggle' }
-
-" nerdtree git support
-Plug 'Xuyuanp/nerdtree-git-plugin',             { 'on':  'NERDTreeToggle' }
-
-" be able to write files with sudo right
-Plug 'chrisbra/SudoEdit.vim',                   { 'on': 'SudoWrite' }
-
-" jump between source and header (.cpp -> .h)
-Plug 'vim-scripts/a.vim',                       { 'on': 'A'}
+Plug 'chrisbra/SudoEdit.vim'
 
 Plug 'whonore/Coqtail'
 
-call plug#end()
+vim.call('plug#end')
 
+vim.cmd [[
 
 filetype plugin indent on
 syntax on
@@ -787,7 +757,7 @@ fun! QuitPrompt()
       if choice == 1 | wq | endif
 endfun
 
-]])
+]]
 
 
 
@@ -835,3 +805,13 @@ require("nvim-tree").setup({
 
 vim.api.nvim_set_keymap('n', '<Leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>nf', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
+
+-- close nvim-tree when it's the last window
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd "quit"
+    end
+  end
+})

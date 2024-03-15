41fff3ff3597bc63847a8cf6c8cbabd7123c3d54
Fabian Beuke <mail@beuke.org>
2024-03-15 23:40:28 +0100

    cleanup neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -44,7 +44,7 @@ Plug 'rhysd/committia.vim'
 -- Changes Vim working directory to project root
 Plug 'airblade/vim-rooter'
 
--- Autoformat JS
+-- Autoformat JS use :Prettier command
 Plug 'prettier/vim-prettier'
 
 -- A collection of language packs for Vim.
@@ -65,6 +65,10 @@ Plug 'jackmort/chatgpt.nvim'
 
 -- TreeSitter Allows Highlighting + Commenting for multiple languages in one File
 Plug 'nvim-treesitter/nvim-treesitter'
+
+--Plug 'williamboman/mason.nvim'
+--Plug 'williamboman/mason-lspconfig.nvim'
+--Plug 'neovim/nvim-lspconfig'
 
 -- ###################################################
 -- Curresntly disabled, might be removed in the future
@@ -130,6 +134,14 @@ Plug 'nvim-treesitter/nvim-treesitter'
 vim.call('plug#end')
 
 
+--require("mason").setup()
+--require("mason-lspconfig").setup {
+--    ensure_installed = { "lua_ls", "rust_analyzer" },
+--}
+--local lspconfig = require('lspconfig')
+--lspconfig.pyright.setup {}
+
+
 require("chatgpt").setup({
         actions_paths = { "~/.config/nvim/helpers/actions.json" },
         open_ai_params = {
@@ -163,7 +175,6 @@ vim.o.cmdheight = 1
 vim.o.complete = '.,w,b,u,t'
 vim.o.confirm = true
 vim.o.cursorline = true
-vim.cmd('set diffopt+=context:99999') -- Appending to a Vim option
 vim.o.encoding = 'utf-8'
 vim.o.expandtab = true
 vim.o.grepprg = 'rg --color never --line-number --no-heading'
@@ -200,12 +211,25 @@ vim.o.wildignore = '*.o,*.obj,*.bak,*.exe,*.py[co],*.swp,*~,*.pyc,.svn,*/.git/*,
 vim.o.wildmenu = true
 vim.o.wildmode = 'longest:list,full'
 vim.o.wrap = true
+vim.o.visualbell = false
+vim.o.errorbells = false
+vim.o.shell = '/bin/bash'
+vim.o.showmatch = true
+vim.o.showmode = true
+vim.o.smartcase = true
+vim.o.title = true
+vim.o.cmdheight = 0
+vim.o.list = true
+vim.o.listchars = 'tab:»-'
+vim.o.tabstop = 4
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.wo.list = true
 vim.wo.listchars = 'tab:»-,trail:·,extends:»,precedes:«'
 vim.wo.cursorline = false
 vim.wo.wrap = true
+vim.wo.scrolloff = 2
+vim.wo.numberwidth = 1
 vim.bo.fileformat = 'unix'
 vim.bo.formatoptions = 'l'
 vim.bo.shiftwidth = 4
@@ -214,34 +238,18 @@ vim.bo.smartindent = true
 vim.bo.softtabstop = 4
 vim.bo.matchpairs = vim.bo.matchpairs .. ',<:>'
 vim.api.nvim_set_keymap('n', '<F11>', '<PasteToggle>', { noremap = true })
-vim.o.visualbell = false
-vim.o.errorbells = false
-vim.wo.scrolloff = 2
-vim.o.shell = '/bin/bash'
-vim.wo.numberwidth = 1
 vim.api.nvim_set_keymap('n', '<F11>', '<paste>', { noremap = true })
-vim.o.showmatch = true
-vim.o.showmode = true
-vim.o.smartcase = true
-vim.o.title = true
-
-vim.cmd("filetype plugin on")
-
--- Enable syntax highlighting
-vim.cmd("syntax on")
-
-vim.cmd("set incsearch")
-
 -- Set mapleader to comma
 vim.g.mapleader = ","
-
+vim.cmd('set diffopt+=context:99999') -- Appending to a Vim option
+vim.cmd("filetype plugin on")
+-- Enable syntax highlighting
+vim.cmd("syntax on")
+vim.cmd("set incsearch")
 -- Disable compatibility mode
 if vim.o.compatible then
     vim.o.nocompatible = true
 end
-
--- set cmdheight=0
-vim.o.cmdheight = 0
 
 -- hi OverLength ctermbg=black ctermfg=red
 -- match OverLength /\%81v.\+/
@@ -256,27 +264,13 @@ vim.api.nvim_create_user_command('W', 'w', {})
 vim.api.nvim_create_user_command('Q', 'q', {})
 
 vim.cmd('colorscheme hybrid')
-
 -- syntax sync minlines=200
 vim.cmd('syntax sync minlines=200')
-
--- syntax on
--- filetype plugin indent on
 vim.cmd('syntax on')
 vim.cmd('filetype plugin indent on')
-
 vim.cmd('hi clear CursorLine')
 vim.cmd('hi clear SpellBad')
--- set relativenumber
-vim.wo.relativenumber = true
 
--- set list
--- set listchars=tab:»-
-vim.o.list = true
-vim.o.listchars = 'tab:»-'
-
--- set tabstop=4
-vim.o.tabstop = 4
 
 -- imap jj <Esc>
 -- map <C-s> <ESC>:w<CR>
@@ -342,7 +336,7 @@ vim.cmd('hi LineNr guibg=#1D1F21')
 
 -- TreeSitter Allows Highlighting + Commenting for multiple languages in one File
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua", "vim" },
+  ensure_installed = { "lua", "vim" , "sql", "python", "svelte", "terraform", "javascript", "html", "css" },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
@@ -372,9 +366,7 @@ autocmd BufWinLeave * call clearmatches()
 " --------------------
 " Plugin Configuration
 " --------------------
-
 let g:tmux_resizer_no_mappings = 1
-
 " Airline settings
 let g:airline_powerline_fonts = 1
 let g:airline_theme='molokai'
@@ -386,10 +378,8 @@ let g:airline#extensions#tabline#fnamemod = ':t'
 let g:airline#extensions#hunks#enabled = 0
 let g:airline#extensions#branch#enabled = 0
 let g:airline#extensions#whitespace#enabled = 0
-
 " Tmux Line settings
 let g:tmuxline_theme = 'zenburn'
-
 " Async Lint Engine settings
 let g:ale_lint_on_text_changed = 0
 let g:ale_sign_error = '>>'
@@ -401,141 +391,61 @@ let g:ale_echo_msg_format = '[%linter%] %s [%severity%]'
 " Write this in your vimrc file
 let g:ale_lint_on_text_changed = 'never'
 let g:ale_lint_on_insert_leave = 0
+let g:ale_python_flake8_options = '--max-line-length=120'
 " You can disable this option too
 " if you don't want linters to run on opening a file
 let g:ale_lint_on_enter = 0
-
 let g:vim_json_syntax_conceal = 0
 let g:prettier#quickfix_enabled = 0
-
 " Silent Rooter otherwise it will echo the directory change
 let g:rooter_silent_chdir = 1
-
 " custom tmux navigator key maps
 let g:tmux_navigator_no_mappings = 1
 let g:tmuxcomplete#trigger = 'omnifunc'
 let g:vim_tags_auto_generate = 1
-
 let g:prettier#autoformat = 0
 let g:prettier#config#tab_width = 4
 let g:prettier#config#use_tabs = 'false'
-
 let g:formatprg_js = "js-beautify"
 let g:formatprg_args_js = "-i %@"
-
 " lilydjwg/colorizer is inefficient for large files
 let g:colorizer_maxlines = 200
-
 let php_sql_query = 1
 let php_htmlInStrings = 1
 let Tlist_Use_Right_Window = 1
 let g:jsx_ext_required = 0
-
 " overcome limit imposed by max height
 let g:ackprg = 'rg --vimgrep'
 set grepprg=rg\ --vimgrep
 let g:tmux_navigator_no_mappings = 1
-
 let g:winresizer_horiz_resize = 1
 let g:vim_markdown_preview_github=1
-
-let g:rg_command = '
-  \ rg --column --line-number --no-heading --fixed-strings --ignore-case --no-ignore --hidden --follow --color "always"
-  \ -g "*.{js,json,php,md,styl,jade,html,config,py,cpp,c,go,hs,rb,conf}"
-  \ -g "!{.git,node_modules,vendor}/*" '
-
-command! -bang -nargs=* F call fzf#vim#grep(g:rg_command .shellescape(<q-args>), 1, <bang>0)
-
-command! -bang -nargs=* Rg
-  \ call fzf#vim#grep(
-  \   'rg --column --line-number --no-heading --color=always '.shellescape(<q-args>), 1,
-  \   <bang>0 ? fzf#vim#with_preview('up:60%')
-  \           : fzf#vim#with_preview('right:50%:hidden', '?'),
-   \   <bang>0)
-
-" Similarly, we can apply it to fzf#vim#grep. To use ripgrep instead of ag:
-command! -bang -nargs=* Rg
-  \ call fzf#vim#grep(
-  \   'rg --column --line-number --no-heading --color=always '.shellescape(<q-args>), 1,
-  \   <bang>0 ? fzf#vim#with_preview('up:60%')
-  \           : fzf#vim#with_preview('right:50%:hidden', '?'),
-  \   <bang>0)
-
-
-autocmd BufRead,BufNewFile ~/.xmonad/* call s:add_xmonad_path()
-function! s:add_xmonad_path()
-  if !exists('b:ghcmod_ghc_options')
-    let b:ghcmod_ghc_options = []
-  endif
-  call add(b:ghcmod_ghc_options, '-i' . expand('~/.xmonad/XMonad'))
-endfunction
-
-let g:haskell_indent_disable=1
-
-let g:ale_linters = {
-\   'haskell': ['hlint', 'ghc-mod', 'hdevtools', 'stack_build', 'stack_ghc'],
-\}
-
+"let g:rg_command = '
+"  \ rg --column --line-number --no-heading --fixed-strings --ignore-case --no-ignore --hidden --follow --color "always"
+"  \ -g "*.{js,json,php,md,styl,jade,html,config,py,cpp,c,go,hs,rb,conf}"
+"j \ -g "!{.git,node_modules,vendor}/*" '
+"command! -bang -nargs=* F call fzf#vim#grep(g:rg_command .shellescape(<q-args>), 1, <bang>0)
+"command! -bang -nargs=* Rg
+"  \ call fzf#vim#grep(
+"  \   'rg --column --line-number --no-heading --color=always '.shellescape(<q-args>), 1,
+"  \   <bang>0 ? fzf#vim#with_preview('up:60%')
+"  \           : fzf#vim#with_preview('right:50%:hidden', '?'),
+"   \   <bang>0)
+"" Similarly, we can apply it to fzf#vim#grep. To use ripgrep instead of ag:
+"command! -bang -nargs=* Rg
+"  \ call fzf#vim#grep(
+"  \   'rg --column --line-number --no-heading --color=always '.shellescape(<q-args>), 1,
+"  \   <bang>0 ? fzf#vim#with_preview('up:60%')
+"  \           : fzf#vim#with_preview('right:50%:hidden', '?'),
+"  \   <bang>0)
 " -------
 " Keymaps
 " -------
-
-imap <TAB> <SPACE><SPACE><SPACE><SPACE>
-nmap <leader>ch1 :set ch=1<CR>
-nmap <leader>ch20 :set ch=20<CR>
-nmap <leader>hit :GhcModTypeInsert<CR>
-nmap <leader>htc :GhcModTypeClear<cr>
-nmap <leader>ht :GhcModType<cr>
-nmap <leader>r :NERDTreeFind<cr>
-nmap <silent> I "=nr2char(getchar())<cr>P
-"This allows for change paste motion cp{motion}
-nmap <silent> cp :set opfunc=ChangePaste<CR>g@
-function! ChangePaste(type, ...)
-    silent exe "normal! `[v`]\"_c"
-    silent exe "normal! p"
-endfunction
-
-noremap <Leader>sen :set spell spelllang=en_us<cr>
-noremap <Leader>gen :set spell spelllang=de_de<cr>
-
-function LightTheme()
-    :silent exec "! sed -i 's/hybrid/Tomorrow/g' ~/.vim-theme-swtich"
-    :silent exec "! sed -i 's/=dark/=light/g' ~/.vim-theme-switch"
-endfunction
-
-function DarkTheme()
-    :silent exec "! sed -i 's/Tomorrow/hybrid/g' ~/.vim-theme-switch"
-    :silent exec "! sed -i 's/=light/=dark/g' ~/.vim-theme-switch"
-endfunction
-
-map s <Plug>(easymotion-overwin-f)
-let g:EasyMotion_keys = 'abcdefghijklmnopqrstuvwxyz'
-
-nmap <silent> gw "_yiw:s/\(\%#\w\+\)\(\_W\+\)\(\w\+\)/\3\2\1/<CR><C-o>:noh<CR>
 nnoremap <leader><Up> ddkP
 nnoremap <leader><Down> ddp
-
-" find other mappings J is in use
-" map K <Plug>(expand_region_expand)
-" map J <Plug>(expand_region_shrink)
-nmap gx <Plug>(openbrowser-smart-search)
-vmap gx <Plug>(openbrowser-smart-search)
-
 nnoremap <C-p> :Files<cr>
 nnoremap <Leader>lb :e#<cr>
-
 nnoremap <Leader>rg :Rg<cr>
-" nnoremap <C-I> i <ESC>r
-
-" o insert line below
-" O insert line above
-" noremap + [
-"
-" nnoremap <Leader>cn :cnext<cr>
-
-
-nnoremap <Leader>fh :History<cr>
-nnoremap <Leader>fb :Buffer<cr>
 nnoremap <Leader>vs :vsplit<cr>
 nnoremap <Leader>hs :split<cr>
 nnoremap <Leader>l :b#<cr>
@@ -543,23 +453,17 @@ nnoremap <Leader>q :q<cr>
 nnoremap <Leader>wq :wq<cr>
 nnoremap <Leader>bd :bd<cr>
 nnoremap <C-j> <C-w>j
-nnoremap <leader>cd :cd<CR>
 nnoremap <silent> <Leader>n :NvimTreeToggle<CR>
-
 nnoremap <C-L> :nohl<CR><C-L>
-
-" the space after ACK here is intentional
+nnoremap ,i i_<Esc>r
 noremap <C-X> :bd<CR>
 noremap <silent> <c-up> :call SwapUp()<CR>
 noremap <silent> <c-down> :call SwapDown()<CR>
 noremap <C-l> :bnext<CR>
 noremap <C-h> :bprevious<CR>
-" fugitive shortcuts
 noremap <Leader>gb :Git blame<cr>
-
 " Fulltext search with ripgrep
 nmap <C-f> :Rg<cr>
-
 nmap <Leader>rv <ESC>:so ~/.vimrc<CR>
 nmap <Leader>pi <ESC>:PlugInstall<CR>
 nmap <Leader>v <ESC>:so ~/.vimrc<CR>
@@ -569,42 +473,28 @@ nmap <S-k> <C-w>wl<CR>
 nmap <S-w> :bd<CR>
 " Jump faster
 nmap <C-j> 4j
-nmap <M-l> <C-w>l
-" Jump to definition (ctags -R)
-nmap <C-b> <C-]>
-nmap o o<ESC>
 nmap <C-k> 4k
-nmap <C-a> :A<CR>
-" nmap <C-o> O<Esc>
+nmap <M-l> <C-w>l
+nmap o o<ESC>
 nmap ^ $
 nmap zz ZZ
-nmap <C-g> <Plug>IMAP_JumpForward
-
+imap jj <Esc><Esc>
 map <C-PageUp> <C-w>wh<CR>
 map <C-PageDown> <C-w>wl<CR>
 map <D-/> <C-_><C-_>
 map Y y$
 map <S-w> <ESC>:q!<CR>
-
 map <F2> ]s
 map <F4> z=
 map <F5> :setlocal spell! spelllang=de_de,en_us<CR>
-
-nnoremap ,i i_<Esc>r
-imap jj <Esc><Esc>
 map <C-s> <ESC>:w<CR>
-
 cnoremap <silent> q<cr>  call QuitPrompt()<cr>
 cnoremap <silent> wq<cr> call QuitPrompt()<cr>
-
 inoremap jk <ESC>
-
 " Ctrl+Delete to delete a word
 inoremap <C-?> <C-W>
-
 silent inoremap <silent> <Tab> <C-n>
 silent inoremap <silent> <S-Tab> <C-p>
-
 vmap ^ $
 
 silent! iunmap (
@@ -612,18 +502,13 @@ silent! iunmap )
 silent! iunmap {
 silent! iunmap }
 
-
-
 " ---------------
 " Autocmd Config
 " ---------------
 augroup vimrc_autocmd
   autocmd!
   au BufNewFile,BufRead,BufEnter *.{tex,txt} setlocal spell spelllang=de_de,en_gb
-  au BufNewFile,BufRead,BufEnter *.pl set filetype=prolog
   au BufNewFile,BufRead,BufEnter *.yml set syntax=yaml
-  au BufRead,BufNewFile,BufEnter *mutt* set filetype=mail
-  au BufNewFile,BufRead *.coffee set filetype=coffee
   au BufNewFile,BufRead *.conf set ft=apache
 
   au FileType javascript setlocal expandtab shiftwidth=4 tabstop=4
@@ -637,73 +522,14 @@ augroup vimrc_autocmd
   au BufEnter * silent! lcd %:p:h
   au FileType mardown set spell spelllang=en_us
 
-  " au VimResized * wincmd =
-
   " Adding automatons for when entering or leaving Vim
   if len(argv()) < 1
     au VimEnter * nested :call LoadSession()
-    au VimLeave * NERDTreeClose
-    au VimLeave * MBEClose
     au VimLeave * :call MakeSession()
   endif
 
 augroup END
 
-autocmd BufRead,BufNewFile ~/.xmonad/* call s:add_xmonad_path()
-function! s:add_xmonad_path()
-  if !exists('b:ghcmod_ghc_options')
-    let b:ghcmod_ghc_options = []
-  endif
-  call add(b:ghcmod_ghc_options, '-i' . expand('~/.xmonad/lib'))
-endfunction
-
-" ask to auto create directory and file if not exsistent on save
-augroup vimrc-auto-mkdir
-  autocmd!
-  autocmd BufWritePre * call s:auto_mkdir(expand('<afile>:p:h'), v:cmdbang)
-  function! s:auto_mkdir(dir, force)
-    if !isdirectory(a:dir)
-          \   && (a:force
-          \       || input("'" . a:dir . "' does not exist. Create? [y/N]") =~? '^y\%[es]$')
-      call mkdir(iconv(a:dir, &encoding, &termencoding), 'p')
-    endif
-  endfunction
-augroup END
-
-" i want always be able to edit the files
-:autocmd BufWinEnter * setlocal modifiable
-
-
-function! Profile()
-  :profile start profile.log
-  :profile func *
-  :profile file *
-endfunction
-
-function! StopProfile()
-  :profile pause
-  :noautocmd qall!
-endfunction
-
-function! MakeSession()
-  let b:sessiondir = $HOME . "/.vim/sessions" . getcwd()
-  if (filewritable(b:sessiondir) != 2)
-    exe 'silent !mkdir -p ' b:sessiondir
-    redraw!
-  endif
-  let b:filename = b:sessiondir . '/session.vim'
-  exe "mksession! " . b:filename
-endfunction
-
-function! LoadSession()
-  let b:sessiondir = $HOME . "/.vim/sessions" . getcwd()
-  let b:sessionfile = b:sessiondir . "/session.vim"
-  if (filereadable(b:sessionfile))
-    exe 'source ' b:sessionfile
-  else
-    echo "No session loaded."
-  endif
-endfunction
 
 fun! QuitPrompt()
       let choice = confirm("Close?", "&yes\n&no", 1)
@@ -712,8 +538,8 @@ endfun
 ]])
 
 
+-- Return cursor to where it was last time closing the file
 vim.api.nvim_create_autocmd({'BufWinEnter'}, {
-  desc = 'return cursor to where it was last time closing the file',
   pattern = '*',
   command = 'silent! normal! g`"zv',
 })
@@ -725,10 +551,8 @@ vim.api.nvim_create_autocmd({'BufWinEnter'}, {
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
-
 require("nvim-tree").setup({
   sort = {
     sorter = "case_sensitive",
@@ -750,9 +574,7 @@ require("nvim-tree").setup({
           empty_open = "EO",
           symlink_open = "SO",
         },
-
       }
-
     }
   },
   filters = {
